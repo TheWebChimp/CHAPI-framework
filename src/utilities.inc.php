ae780aaff9f60586e0bbd1d93d7baa6d777d8108
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2021-10-25 09:44:58 -0500

    Adding full utility functions

--- src/utilities.inc.php
+++ src/utilities.inc.php
@@ -1,5 +1,9 @@
 <?php
 
+	/**
+	 * Pretty-print an array or object
+	 * @param  mixed $a Array or object
+	 */
 	if(!function_exists('print_a')) {
 		function print_a($a) {
 			print('<pre>');
@@ -8,11 +12,119 @@
 		}
 	}
 
+	/**
+	 * Get an item from an array/object, or a default value if it's not set
+	 * @param  mixed $var      Array or object
+	 * @param  mixed $key      Key or index, depending on the array/object
+	 * @param  mixed $default  A default value to return if the item it's not in the array/object
+	 * @return mixed           The requested item (if present) or the default value
+	 */
 	if(!function_exists('get_item')) {
 		function get_item($var, $key, $default = '') {
 			return is_object($var) ?
 				( isset( $var->$key ) ? $var->$key : $default ) :
 				( isset( $var[$key] ) ? $var[$key] : $default );
+		}
+	}
+
+	/**
+	 * Convert a shorthand byte value from a PHP configuration directive to an integer value
+	 * @param    string   $value
+	 * @return   int
+	 */
+	if(!function_exists('convert_bytes')) {
+		function convert_bytes( $value ) {
+			if ( is_numeric( $value ) ) {
+				return $value;
+			} else {
+				$value_length = strlen( $value );
+				$qty = substr( $value, 0, $value_length - 1 );
+				$unit = strtolower( substr( $value, $value_length - 1 ) );
+				switch ( $unit ) {
+					case 'k':
+						$qty *= 1024;
+						break;
+					case 'm':
+						$qty *= 1048576;
+						break;
+					case 'g':
+						$qty *= 1073741824;
+						break;
+				}
+				return $qty;
+			}
+		}
+	}
+
+	/**
+	 * Convert camelCase to snake_case
+	 * @param  string $val Original string
+	 * @return string      The converted string
+	 */
+	if(!function_exists('camel_to_snake')) {
+		function camel_to_snake($val) {
+			$val = preg_replace_callback('/[A-Z]/', '_camel_to_snake_callback', $val);
+			return ltrim($val, '_');
+		}
+
+		function _camel_to_snake_callback($match) {
+			return "_" . strtolower($match[0]);
+		}
+	}
+
+
+	/**
+	 * Convert camelCase to dash-case
+	 * @param  string $val Original string
+	 * @return string      The converted string
+	 */
+	if(!function_exists('camel_to_dash')) {
+		function camel_to_dash($val) {
+			$val = preg_replace_callback('/[A-Z]/', '_camel_to_dash_callback', $val);
+			return ltrim($val, '-');
+		}
+
+		function _camel_to_dash_callback($match) {
+			return "-" . strtolower($match[0]);
+		}
+	}
+
+	/**
+	 * Convert snake_case to camelCase
+	 * @param  string $val Original string
+	 * @return string      The converted string
+	 */
+	if(!function_exists('snake_to_camel')) {
+		function snake_to_camel($val) {
+			$val = str_replace(' ', '', ucwords(str_replace('_', ' ', $val)));
+			$val = strtolower(substr($val, 0, 1)).substr($val, 1);
+			return $val;
+		}
+	}
+
+	/**
+	 * Convert dash-case to camelCase
+	 * @param  string $val Original string
+	 * @return string      The converted string
+	 */
+	if(!function_exists('dash_to_camel')) {
+		function dash_to_camel($val) {
+			$val = str_replace(' ', '', ucwords(str_replace('-', ' ', $val)));
+			$val = strtolower(substr($val, 0, 1)).substr($val, 1);
+			return $val;
+		}
+	}
+
+	/**
+	 * Get the singular or plural form of a word based on a quantity
+	 * @param  number $number   The quantity
+	 * @param  string $singular Singular form of the word
+	 * @param  string $plural   Plural form of the word
+	 * @return string           The appropiate form of the word
+	 */
+	if(!function_exists('singular_plural')) {
+		function singular_plural($number, $singular, $plural = '') {
+			return $number == 1 ? $singular : ($plural ? $plural : "{$singular}s");
 		}
 	}
 ?>
