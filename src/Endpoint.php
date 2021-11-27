323251b1d6bce5fd794b9e33495c88628c937b34
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2021-11-26 19:22:44 -0600

    Converting Edpoint class into a Base Endpoint Class

--- src/Endpoint.php
+++ src/Endpoint.php
@@ -15,7 +15,40 @@
 		 * Constructor
 		 */
 		function __construct() {
+
+			global $app;
+
+			$this->message = '';
+			$this->data = [];
+			$this->properties = [];
+			$this->status = 404;
+			$this->result = 'error';
+
+			$this->router = $app->router;
+			$this->request = $app->router->getRequest();
+			$this->response = $app->router->getResponse();
+
+			$called_class =  get_called_class();
+
+			$this->plural = str_replace('Endpoint', '', $called_class);
+			$this->singular = rtrim($this->plural, 's');
+
 			$this->init();
+		}
+
+		function addRoute($route, $functName, $method = '*') {
+			$this->router->prepend('/' . strtolower($this->plural) . '/' . $route, $functName, $method);
+		}
+
+		function respond() {
+			$this->response->ajaxRespond($this->result, $this->data, $this->message, $this->properties);
+			exit;
+		}
+
+		function getItemById($id) {
+
+			$item = $this->plural::getById($id);
+			return $item;
 		}
 
 		/**
@@ -23,8 +56,85 @@
 		 */
 		abstract function init();
 
-		function dispatchAction($id) {
-			return false;
+		/**
+		 * Lists all elements from a class
+		 */
+		function list() {
+
+			//Items to show, we can define the number of items via the show query param
+			$show = $this->request->get('show', 100);
+			$show = $show ?: 100;
+
+			//Pages to show, we can define the page number via the page query param
+			$page = $this->request->get('page', 1);
+			$page = $page ?: 1;
+
+			//Sorting defined via the sort query param
+			$sort = $this->request->get('sort', 'asc');
+			$sort = $sort ?: 'asc';
+
+			//Sort by parameter defined via the by query param
+			$by = $this->request->get('by', 'id');
+			$by = $by ?: 'id';
+
+			$args = [ 'show' => $show, 'page' => $page, 'sort' => $sort, 'by' => $by, 'args' => [] ];
+
+			//Defines if we want to fetch metas or not
+			$fetch_metas = $this->request->get('fetch_metas');
+			if($fetch_metas) {
+
+				if($fetch_metas != 1) $fetch_metas = explode(',', $fetch_metas);
+				$args['args']['fetch_metas'] = $fetch_metas;
+			}
+
+			//Automagically add conditions based on get params
+			foreach($_GET as $key => $value) {
+
+				if(in_array($key, $this->plural::getTableFields())) {
+					$args['conditions'][] = "`{$key}` = '{$value}'";
+				}
+			}
+
+			$items = $this->plural::all($args);
+
+			$this->data = $items;
+
+			$count = $this->plural::count($args['conditions']);
+			$pages = ceil($count / $show);
+
+			$this->properties['current_page'] = (int) $page;
+			$this->properties['per_page'] = (int) $show;
+			$this->properties['last_page'] = (int) $pages;
+			$this->properties['count'] = (int) $count;
+			$this->properties['sort'] = $sort;
+			$this->properties['by'] = $by;
+			$this->properties['total'] = (int) $pages;
+
+			if(count($items)) {
+
+				$this->result = 'success';
+			}
+		}
+
+		function single($id) {
+
+			$args = ['args' => []];
+
+			//Defines if we want to fetch metas or not
+			$fetch_metas = $this->request->get('fetch_metas');
+			if($fetch_metas) {
+
+				if($fetch_metas != 1) $fetch_metas = explode(',', $fetch_metas);
+				$args['args']['fetch_metas'] = $fetch_metas;
+			}
+
+			$item = $this->getItemById($id, $args);
+
+			if($item) {
+
+				$this->data = $item;
+				$this->result = 'success';
+			}
 		}
 	}
 ?>
